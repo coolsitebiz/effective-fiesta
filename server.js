9d8cd0a62f09ba2442b330663a01e6a368282f3c
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2020-07-03 14:08:43 -0700

    added eslint and index.html placeholder

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const chalk = require('chalk');
+const debug = require('debug')('server');
+const morgan = require('morgan');
+const path = require('path');
 
+const port = 3000;
 const app = express();
 
-app.get('/', function( req, res ) {
-    res.send('Hello');
-})
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
-app.listen(3000, () => {
-    console.log(chalk.green('Server running...'));
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.listen(port, () => {
+  debug(chalk.green(`Server listening on port ${port}`));
+});
