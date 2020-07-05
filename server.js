3218844148b2e316f1d6d1e8beb0fa30a300671c
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2020-07-05 00:11:14 -0700

    added ejs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const debug = require('debug')('server');
 const morgan = require('morgan');
 const path = require('path');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,9 +12,11 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index', { list: ['item 1', 'item 2', 'item 3'], title: 'TITLE FOR THE PAGE' });
 });
 
 app.listen(port, () => {
