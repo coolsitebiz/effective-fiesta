a770381d5ae0adf744bb8ae13fd46b7ffad8eddd
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2020-07-20 00:22:28 -0700

    added data load for fakedata in local mongo

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const users = require('../../fakedata');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminroutes');
+const getUsers = require('../../fakedata');
 
 const adminRouter = express.Router();
 
 function router(data) {
   adminRouter.route('/')
     .get((req, res) => {
-      res.send(users(25));
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'certApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url, { useUnifiedTopology: true });
+          debug('Connected to database server');
+          const users = getUsers(25);
+          const db = client.db(dbName);
+          const response = await db.collection('users').insertMany(users);
+          res.json(response);
+        } catch (err) {
+          debug(err.stack);
+        }
+        client.close();
+      }());
     });
   return adminRouter;
 }
